4140b99ace59db25f86c0e65d20924b9187034be
William Gurecky <william.gurecky@gmail.com>
2018-10-02 21:15:13 -0400

    adds setup.py

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+setup(
+    name = "bipymc",
+    version = "0.1",
+    packages = find_packages(),
+    install_requires = ['numpy>=1.7.0', 'scipy>=0.12.0'],
+    package_data = { '': ['*.txt'] },
+    author = 'William Gurecky',
+    license = "BSD3",
+    author_email = "william.gurecky@gmail.com",
+)
