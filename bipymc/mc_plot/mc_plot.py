e6d47eac79ba5a763090fadbad050a71b744ed71
William Gurecky <william.gurecky@gmail.com>
2018-11-05 17:39:50 -0500

    improved plot

--- bipymc/mc_plot/mc_plot.py
+++ bipymc/mc_plot/mc_plot.py
@@ -1,13 +1,32 @@
 import corner
 import matplotlib.pyplot as pl
 from matplotlib.ticker import MaxNLocator
+import numpy as np
 
 
-def plot_mcmc_params(samples, labels, savefig='corner_plot.png', truths=None):
+def plot_mcmc_params(samples, labels, savefig='corner_plot.png', truths=None, show_titles=False):
     fig = corner.corner(samples, labels=labels,
-            truths=truths, use_math_text=True, show_titles=True,
-            title_fmt='0.3e', title_kwargs={'fontsize': 9})
+            truths=truths, use_math_text=True, show_titles=show_titles,
+            title_fmt='0.2e', title_kwargs={'fontsize': 9})
+
+    ndims = samples.shape[1]
+    axes = np.array(fig.axes).reshape((ndims, ndims))
+    for i in range(ndims):
+        ax = axes[i, i]
+        ax_data = samples[:, i]
+        mean_line(ax, ax_data)
     fig.savefig(savefig)
+
+
+def mean_line(ax, x, **kwargs):
+    # ax = plt.gca()
+    mean = np.average(x)
+    sd = np.std(x)
+    ax.axvline(mean, ls='--', c='k', alpha=0.5)
+    ax.annotate(r"$\mu$ = {:.3e}".format(mean),
+                xy=(0.71, 0.93), xycoords=ax.transAxes)
+    ax.annotate(r"$\sigma$ = {:.3e}".format(sd),
+                xy=(0.71, 0.835), xycoords=ax.transAxes)
 
 
 def plot_mcmc_chain(samples, labels, savefig, truths=None):
