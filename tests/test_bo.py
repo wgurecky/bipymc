9abe6ad4da46b10ca3a8f13a740036cd03703ce4
William Gurecky <william.gurecky@gmail.com>
2020-09-01 04:52:42 -0500

    wip adding bo test to ci

---
+++ tests/test_bo.py
@@ -0,0 +1,10 @@
+from __future__ import division, print_function
+import numpy as np
+import unittest
+from bipymc.gp.gpr import gp_regressor
+np.random.seed(42)
+
+
+class TestBayesOpt(unittest.TestCase):
+    def test_1d_bo(self):
+        pass
